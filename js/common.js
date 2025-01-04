0270f23509dc2211b5f00b5273dd6e374a74375d
wb96choi <choi4601280@gmail.com>
2025-01-04 21:37:29 +0900

    setting

---
+++ js/common.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // $(document).ready(function() {}); >> jQuery
+    
+	// if(document.querySelector('.check_box_container')!=undefined) {
+    //     require(['../ejs/checkBoxAssessment.js'], function() {
+    //     });
+    // }
+
+});
